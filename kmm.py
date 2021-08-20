2a391bed2d0dc8a01e9138157c678affe63a1b6f
Asma <87402543+miss-683@users.noreply.github.com>
2021-08-20 10:39:05 +0430

    Add files via upload

---
+++ kmm.py
@@ -0,0 +1,13 @@
+a= int(input())
+b= int(input())
+
+#if a > b :
+  #  print("the second number must be bigger than the first number.. try again")
+
+for i in range(b, a*b +1):
+    if a % i == 0 and b % i == 0 :
+        i= kmm
+        break
+
+
+print(i)
